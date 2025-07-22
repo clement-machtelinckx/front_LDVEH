d3e3dee504af21523d3bb1ee8f1547f8ca945990
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2025-07-22 09:48:31 +0200

    confic

--- constants/api.ts
+++ constants/api.ts
@@ -1,12 +1,15 @@
 // constants/api.ts
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 const URL = 'https://ldveh1.osc-fr1.scalingo.io';
 // const URL = 'https://localhost:8000';
 // const URL = 'https://localhost:8000'; // For local development, uncomment this line and comment the above line
 // const URL = 'http://localhost:8000'; // For local development without SSL
 // const URL = 'https://ldveh1.osc-fr1.scalingo.io'; // For production on Scalingo
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 // const URL = 'https://ldveh1.osc-fr1.scalingo.io';
