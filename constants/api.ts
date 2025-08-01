f2dcbc8c2f0ea7c40da5ba56e633f2dd36f8acbc
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2025-08-01 09:41:08 +0200

    chang api for prod add owner on eas for CI/CD

--- constants/api.ts
+++ constants/api.ts
@@ -1,9 +1,13 @@
 // constants/api.ts
-// const URL = 'https://ldveh1.osc-fr1.scalingo.io';
-const URL = 'https://localhost:8000';
+const URL = 'https://ldveh1.osc-fr1.scalingo.io';
+// const URL = 'https://localhost:8000';
 // const URL = 'https://localhost:8000'; // For local development, uncomment this line and comment the above line
 // const URL = 'http://localhost:8000'; // For local development without SSL
 // const URL = 'https://ldveh1.osc-fr1.scalingo.io'; // For production on Scalingo
 
 export const BASE_URL = URL;
 export const API_URL = `${URL}/api`;
+
+
+
+// TODO : mettre en place les variable d'env pour prod et dev 
