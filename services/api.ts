0bfc450045ff8f8fc178984a50e11fe81689de71
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2026-03-05 10:09:25 +0100

    fix const

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 // services/api.ts
 
 // a check si ce fichier sert a un truc si non sa dégage
-export const API_BASE_URL = 'http://10.0.2.2:3000'; // 🔁 change si tu testes sur un vrai tel
+export const API_BASE_URL = 'http://10.0.2.2:3000';
 
 export async function fetchBooks() {
   const res = await fetch(`${API_BASE_URL}/books`);
